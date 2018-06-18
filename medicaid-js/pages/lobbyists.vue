7cd79b3a726c0744218bba8c0f359f6c50b74257
juliarenee <jdonheiser@gmail.com>
2018-06-18 18:02:09 -0400

    Yikes this is ugly

--- medicaid-js/pages/lobbyists.vue
+++ medicaid-js/pages/lobbyists.vue
@@ -10,127 +10,44 @@
 /* eslint-disable */
 import data from '~/assets/lobbyist-ratios.json';
 import Highcharts from 'highcharts';
+import postal from 'journalize';
 export default {
+
     methods: {
-        makeDates(floatArr) {
-            let tar = [];
-            for (let i = 0; i < floatArr.length; i++) {
-                tar[i] = Date.UTC(floatArr[i],0,1);
-            }
-            return tar;
-        },
         makeSeries(mydata) {
             const tar = [];
-            let seriesI = {};
-            seriesI.name = "Total Spending";
-            seriesI.data = mydata.data[1];
-            seriesI.pointStart = Date.UTC(2008,0,1); 
-            seriesI.pointIntervalUnit = 'year'
-            seriesI.marker = { symbol: 'circle', radius: 3 };
-            tar.push(seriesI);
+            for (let i = 0; i < mydata.index.length; i++) {
+                let seriesI = {};
+                seriesI.name = mydata.index[0];
+                seriesI.data = mydata.data[0];
+                tar.push(seriesI);
+            }
             return tar;
         }
     },
-    mounted() {
-        let cols = this.makeDates(data.columns)
-        let dSeries = this.makeSeries(data);
-        Highcharts.setOptions({
-            chart: {
-                style: {
-                    fontFamily: 'tablet-gothic-narrow'
-                }
-            }
-        });
-        Highcharts.chart('chart', {
-            colors: ['#51AADE'],
-            title: {
-                display: 'null',
-                text: ' ',
-                x: -20 // center
-            },
-            subtitle: {
-                display: 'null',
-                text: ' ',
-                x: -20
-            },
-            credits: {
-                enabled: false
-            },
-            xAxis: {
 
-                plotLines: [
-                    {
-                        value: Date.UTC(2010),
-                        width: 1.5,
-                        color: '#bebebe',
-                        label: {
-                            text: 'ACA passed',
-                            style: {
-                                color: '#737373'
-                            }
-                        }
-                    },
-                    {
-                        value: Date.UTC(2014),
-                        width: 1.5,
-                        color: '#bebebe',
-                        label: {
-                            text: 'Medicaid expansion',
-                            style: {
-                                color: '#737373'
-                            }
-                        }                        
-                    }                    
-                ],
-                labels: {
-                    style: {
-                        fontSize: '10px'
-                    }
-                },
+    mounted() {
+        Highcharts.chart('chart', {
+            chart: {
                 type: 'bar'
             },
-            yAxis: {
+            xAxis: {
+                categories: data.states,
                 title: {
-                    text: ''
-                },
-                labels: {
-                    formatter() {
-                        return `$${Highcharts.numberFormat(this.value, 0)}T`;
-                    }
-                },
-                plotLines: [
-                    {
-                        value: 0,
-                        width: 1.5,
-                        color: '#bebebe'
-                    }
-                ]
-            },
-            tooltip: {
-                pointFormat: '{series.name}:<b>${point.y:.1f} trillion</b>',
-                crosshairs: true
-            },
-            legend: {
-                enabled: true,
-                align: 'right',
-                verticalAlign: 'bottom',
-                borderWidth: 0
-            },
-            series: dSeries,
-            title: {
-                text: '<b>... which means Drug Payouts are Increasing, too</b>',
-                align: 'left'
-            },
-            subtitle: {
-                text: 'State and federal Medicaid drug spending in trillions of dollars',
-                align: 'left',
-                style: {
-                    fontSize: '15px'
+                    text: null
                 }
             },
-            style: {
-                fontFamily: 'tablet-gothic-narrow'
-            }
+            yAxis: {
+                data: data.index[1],
+                min: 0,
+                title: {
+                    text: null
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            series: this.makeSeries(data)
         });
     }
 };
@@ -139,7 +56,7 @@ export default {
 
 <style scoped>
 #chart {
-    height: 400px;
+    height: 1000px;
     margin: 0 auto;
     padding-bottom: 20px;
 }
